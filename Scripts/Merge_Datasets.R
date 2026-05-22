bb8b522e073f98f2c225c6c48c13ef5224068cd5
Samuel-Howest1 <samuel.gundes@student.howest.be>
2026-05-22 08:42:02 +0200

    Successfully merged all Datasets and removed any found contamination

--- Scripts/Merge_Datasets.R
+++ Scripts/Merge_Datasets.R
@@ -8,8 +8,10 @@ library("renv")
 library("Seurat")
 library("patchwork")
 library("harmony")
-#library("readxl")
-#library("presto") 
+library("readxl")
+library("presto")
+library("ggrastr")
+library("openxlsx")
 
 # Load Seuratobjects
 
@@ -67,7 +69,7 @@ seuratObjT <- RunPCA(seuratObjT,
                     reduction.key = "rnaPC_int_")
 
 
-
+saveRDS(seuratObjT,"SeuratObjT_tmp_save_after_PCA")
 # Tested with 100 Dims to seleceted Dims for Merge of all datasets
 # Conclusion --> use 40 Dims for merge of all datasets
 Elbow <- ElbowPlot(seuratObjT, reduction = "RNA_pca_int",ndims = 100)
@@ -77,64 +79,91 @@ Elbow
 seuratObjT <- FindNeighbors(seuratObjT, dims = 1:40, reduction = "RNA_pca_int")
 
 #Starting with high resolution to separate contamination
-seuratObjT <- FindClusters(seuratObjT, resolution = 1)
+seuratObjT <- FindClusters(seuratObjT, resolution = 1.2)
 seuratObjT <- RunUMAP(seuratObjT, dims = 1:40,reduction = "RNA_pca_int", reduction.name = "RNA_umap" )
+
+pdf("Graphs_SeuratObjT_all_DataSet_Merged_BEFORE.pdf", width = 14,height = 10)
 DimPlot(seuratObjT,label = T)
 DimPlot(seuratObjT,label = T,group.by = "sctype_classification")
 DimPlot(seuratObjT,label = T,group.by = "HTO_GUESS")
 DimPlot(seuratObjT,label = T,group.by = "scDblFinder_class")
 DimPlot(seuratObjT,label = T,group.by = "orig.ident")
+dev.off()
 
-saveRDS(seuratObjT,"SeuratObjT_tmp_save_")
-
-
-
+###################################################################################33
 AnnotData <- read_xlsx(path = "/Users/irc/Desktop/Interschip Bioinformatis 2025-2026/DC-Atlas-VIB-Scripts/CDC Annotatie.xlsx")
 AnnotData
 
-plot_list <- list()
-pdf("Graphs_Processing_Merged_Data", width = 14,height = 10)
+# NK cell gene Klrb1c cause errors, because there are 12 000 cells that don't have it as a feautre
+AnnotData$geneSymbolmore1[9] <- "Gzmb"
+
+#Memory CD4+ T-cell: Ccr10  cause errors, because there are 12 000 cells that don't have it as a feautre
+AnnotData$geneSymbolmore1[11] <- "Cd3d,Cd4,S100a4,Il7r"
+
+pdf("Graphs_Processing_Merged_Data.pdf", width = 14,height = 10)
 for (i in 1:nrow(AnnotData)){
   ExcelMarker <- unlist(strsplit(AnnotData$geneSymbolmore1[row.names(AnnotData)==i],","))
 
   # Featureplot best used for looking per gene
-  print(AnnotTitle)
   for (g in ExcelMarker){
     AnnotTitle <- paste0(AnnotData$cellName[row.names(AnnotData)==i],": ",g)
+    print(AnnotTitle)
     
-    p <- FeaturePlot(object = seuratObj, features = g, cols = c("grey", "blue"), 
+    p <- FeaturePlot(object = seuratObjT, features = g, cols = c("grey", "blue"), 
                    reduction = "RNA_umap", min.cutoff = 'q2', max.cutoff = 'q98', pt.size = 0.7,order = FALSE)
     p_combined <- p + plot_annotation(title = AnnotTitle)
     
-    p
+    print(p_combined)
   }
 }
-
 dev.off()
 
-# Checking If its is CDC1
-PlotUnknown1 <- FeaturePlot(seuratObjT,features = c("Cd81","Gcsam","Xcr1","Irf8"))
-PlotUnknown1 <- PlotUnknown1 +plot_annotation(title = "Controling if CDC1")
+# True Order
 
-# Checking if its is CDC2
-PlotUnknown2 <- FeaturePlot(seuratObjT,features = c("S100a4"))
-PlotUnknown2 <- PlotUnknown2 +plot_annotation(title = "Controling if CDC2")
+pdf("Graphs_Processing_Merged_Data_TRUE_ORDER3.pdf", width = 14,height = 10)
+for (i in 1:nrow(AnnotData)){
+  ExcelMarker <- unlist(strsplit(AnnotData$geneSymbolmore1[row.names(AnnotData)==i],","))
+  
+  # Featureplot best used for looking per gene
+  for (g in ExcelMarker){
+    AnnotTitle <- paste0(AnnotData$cellName[row.names(AnnotData)==i],": ",g)
+    print(AnnotTitle)
+    
+    p <- FeaturePlot(object = seuratObjT, features = g, cols = c("grey", "blue"), 
+                     reduction = "RNA_umap", min.cutoff = 'q2', max.cutoff = 'q98', pt.size = 0.7,order = TRUE)
+    p_combined <- p + plot_annotation(title = AnnotTitle)
+    
+    print(p_combined)
+  }
+}
+dev.off()
+##################################################################################
+# Notes for filtering
 
-# Cheching if Migratory CDC1 
-PlotUnknown3 <- FeaturePlot(seuratObjT,features = c("Cd81","Gcsam","Laptm4b","Ncoa7","Ccr7"))
-PlotUnknown3 <- PlotUnknown3 +plot_annotation(title = "Controling if Migartory CDC1")
+#Cluster 23: High S100a4 expression ---> CDC2
 
-PlotUnknown1
-PlotUnknown2
-PlotUnknown3
+# Checking pre-mature cells
+# Pre-mature cells between prolifrqting and CDC1 ---> not a separate cluster 
+Ccr2 <- FeaturePlot(object = seuratObjT, features = "Ccr2", cols = c("grey", "blue"), 
+            reduction = "RNA_umap", min.cutoff = 'q2', max.cutoff = 'q98', pt.size = 0.7,order = TRUE,raster = FALSE)
+Ccr2
 
+# Cluster 24 and 27----> RBC
 #Biomarkers <- FindAllMarkers(seuratObjT, only.pos = TRUE, test.use="wilcox")
 
+# Migratory CDC1 and 2 toghter zith all cell
+# For Migrqtory CDC2 ---> of all datasets VBO005 regestirs has having a lot of Migratory CDC2, Why? 
 
+# cant look at BioMarkers have to look after integration
 
+#############################################################################
+# Removing Cells
 
+# CDC2 are conclusif as being Cluster 23
 
+# RBC are conclusif as being Cluster 24 and 27
 
+# Cluster 30 is certainly NOT a CDC1, It looks like to be q T-cell of a kind
 
 # seuratObjT for integration
 
