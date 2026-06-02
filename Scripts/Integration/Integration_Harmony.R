3f0f7388945eb3e657a02f6a7250e89e44428963
Samuel-Howest1 <samuel.gundes@student.howest.be>
2026-06-02 08:53:16 +0200

    Update Integration_Harmony.R

--- Scripts/Integration/Integration_Harmony.R
+++ Scripts/Integration/Integration_Harmony.R
@@ -14,37 +14,37 @@ library("openxlsx")
 # for script
 #setwd(/srv/data/local/samuelg/)
 
-output <- "/srv/data/local/samuelg/Output"
-seuratObjT <- readRDS("Subset_Merged_seurat500.rds")
+# output <- "/srv/data/local/samuelg/Output"
+seuratObjT <- readRDS("/Users/irc/Desktop/Interschip Bioinformatis 2025-2026/SeuratObjT_Before_Integration_V2")
 ###################################################################
 #folder <- "C:/Users/irc/Desktop/Interschip Bioinformatis 2025-2026/Pre and Post processing Results/"
 #DataList <- list.files(folder)
 # Removing VBO_merge out of DataList/ comment out if not neede
 #DataList <- DataList[-8]
 
-#Test Only JVE
-#DataList <- DataList[c(1:2,10:11)]
-#start <- 1
-#Only CDC1
-#for (i in DataList){
-#  print(i)
-#  #Starting seuratobject
-#  if (start == 1){
-#    seuratObjT <- readRDS(paste0(folder,i,"/Post-process/SeuratObj_Post-Process_CDC1_",i,".rds"))
-#    #    seuratObjT <- seuratObjT
-#    start <- start + 1
-#    colnames(seuratObjT)<- paste0(colnames(seuratObjT),"_",i)
-#    seuratObjT$orig.ident <- i
-#  }
-#  
-#  else{
-    tmp <- readRDS(paste0(folder,i,"/Post-process/SeuratObj_Post-Process_CDC1_",i,".rds"))
-    #  tmp <- tmp[,1:500] 
-    tmp$orig.ident <- i
-    colnames(tmp)<- paste0(colnames(tmp),"_",i)
-    seuratObjT <- merge(seuratObjT,tmp)
-  }
-#}
+# #Test Only JVE
+# #DataList <- DataList[c(1:2,10:11)]
+# #start <- 1
+# #Only CDC1
+# #for (i in DataList){
+# #  print(i)
+# #  #Starting seuratobject
+# #  if (start == 1){
+# #    seuratObjT <- readRDS(paste0(folder,i,"/Post-process/SeuratObj_Post-Process_CDC1_",i,".rds"))
+# #    #    seuratObjT <- seuratObjT
+# #    start <- start + 1
+# #    colnames(seuratObjT)<- paste0(colnames(seuratObjT),"_",i)
+# #    seuratObjT$orig.ident <- i
+# #  }
+# #  
+# #  else{
+#     tmp <- readRDS(paste0(folder,i,"/Post-process/SeuratObj_Post-Process_CDC1_",i,".rds"))
+#     #  tmp <- tmp[,1:500] 
+#     tmp$orig.ident <- i
+#     colnames(tmp)<- paste0(colnames(tmp),"_",i)
+#     seuratObjT <- merge(seuratObjT,tmp)
+#   }
+# #}
 ###################################33
 # fix error
 # The annontation of the given Object VBO_merge was kept in Annotation_VBO, thus we can easily fix this
@@ -58,8 +58,8 @@ seuratObjT@meta.data$sctype_classification[seuratObjT$Annotation_VBO == "Early m
 
 ########################################
 
-rm(tmp)
-gc()
+# rm(tmp)
+# gc()
 #################################################################
 # Adding Metadata
 experiment_map <- c(
@@ -124,15 +124,24 @@ seuratObjT <- RunPCA(seuratObjT,
                      reduction.key = "rnaPC_int_")
 
 ###############################################################################################
+pca_embed <- Embeddings(seuratObjT,"RNA_pca_int")
+meta_data <- seuratObjT@meta.data
+
 BenchResult <- bench::mark(
-seuratObjT <- RunHarmony(seuratObjT, 
-                         group.by.vars =c("treatment","experiment"), 
-                         plot_convergence =TRUE, 
-                         reduction.use ="RNA_pca_int", 
-                         theta=c(2,2),
-                         sigma=0.2,
-                         lambda=1,
-                         verbose=TRUE),memory = F
+Harm_int <- RunHarmony(data_mat = pca_embed,
+                         meta_data = meta_data,
+                         vars_use = "treatment",
+                         theta = 2,
+                         sigma = 0.2,
+                         lambda = 1,
+                         plot_convergence = TRUE,
+                         verbose = TRUE),memory = F
+)
+
+seuratObjT[["harmony"]] <- CreateDimReducObject(
+  embeddings = Harm_int,
+  key = "harmony_",
+  assay = DefaultAssay(seuratObjT)
 )
 # Reduce max.itration?
 # Specific number of cluster?
@@ -142,7 +151,6 @@ seuratObjT <- FindNeighbors(seuratObjT,reduction = "harmony",dims = 1:40)
 seuratObjT <- FindClusters(seuratObjT, resolution = 1.2)
 seuratObjT <- RunUMAP(seuratObjT,reduction = "harmony",dims = 1:40,reduction.name = "harmony_umap")
 
-DimPlot(seuratObjT,label = T,group.by = "sctype_classification",reduction = "harmony_umap")
 
                            
 #######################################################################""
@@ -212,7 +220,7 @@ ASWData <- as.data.frame(ASWScore)
 
 ###################################
 # Set output
-setwd(output)
+# setwd(output)
 ###################################
 
 wb <- createWorkbook()
@@ -242,7 +250,7 @@ saveWorkbook(
 ##############################"
 # Plots pdf
 Plotslist <-c("orig.ident","experiment","treatment","sctype_classification","seurat_clusters","scDblFinder_class")
-pdf("./Harmomy/Results/Graphs_Harmony_Integration", width = 10,height = 8)
+pdf("Graphs_Harmony_Integration_VIB_Lap", width = 10,height = 8)
 for (i in Plotslist){
 
     AnnotTitle <- paste0("Plot Harmony integration: ",i)
@@ -256,4 +264,4 @@ for (i in Plotslist){
 }
 dev.off()
 
-saveRDS(seuratObjT,"./Harmomy/Object/SeurqtObjT_Harmomy_Subset.rds")
+saveRDS(seuratObjT,"SeurqtObjT_Harmomy_only.rds")
