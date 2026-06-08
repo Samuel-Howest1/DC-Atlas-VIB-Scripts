394ad2645c689fdd92e119636fb42db89e5246c1
Samuel-Howest1 <samuel.gundes@student.howest.be>
2026-06-08 08:42:59 +0200

    Corrected the gene used and fixed the loop of Feature_plot_loop

--- Scripts/Integration and Multimodel/Feature_plot_loop.R
+++ Scripts/Integration and Multimodel/Feature_plot_loop.R
@@ -3,47 +3,40 @@ library("Seurat")
 library("ggplot2")
 library(patchwork)
 
-# late -mature =  Cd63, Fscn1, Il4i1, or Socs2,
-# Early-mature  = Cxcl10,Cxcl9, Iigp1, Ifi47, Gbp2, and Gbp5
-# Later Imature = CD207+ CD103+,Apol7c,Apoe,Dnase1l3
-# Early immature =CD62L,Fdps
-# Pre-cdc1 = Ccr2, Fcer1g, and Cd24a
+Pre_cDC1 <- c("Ccr2", "Fcer1g", "Cd24a", "Vim")
+Early_Immature <- c("Sell", "Creld2", "Pdia4")
+Later_Immature <- c("Cd207", "Itgae", "Apol7c", "Apoe", "Dnase1l3", "Cadm1", "Xcr1", "Cd83", "Cd86")
+Early_Mature <- c("Cxcl10", "Cxcl9", "Iigp1", "Ifi47", "Gbp2", "Gbp5", "Cd40")
+Late_Mature <- c("Cd63", "Fscn1", "Il4i1", "Socs2", "Ccr7")
+
 
 Seuratplot <- readRDS("/Users/irc/Desktop/Interschip Bioinformatis 2025-2026/SeuratObjT_Harmony_Treat_Exp_Theta_ALL_3.rds")
 
 
-Lat_mat <-  list("Cd63", "Fscn1", "Il4i1", "Socs2")
-Ear_Mat <-  list("Cxcl10", "Cxcl9", "Iigp1", "Ifi47", "Gbp2", "Gbp5")
-Lat_Im <-   list("Cd207", "Cd103", "Apol7c", "Apoe", "Dnase1l3")
-Ear_IM <-   list("Cd62L", "Fdps")
-Pre_CDC1 <- list("Ccr2", "Fcer1g", "Cd24a")
-
 origlist <- unique(Seuratplot@meta.data$orig.ident)
 pdf("Harmony_CDC1_Markers_Theta_3", width = 14,height = 10)
-for (i in Lat_mat){
-  plot_list <- list()
-    for (g in origlist){
-    AnnotTitle <- paste0("Late-mature:",i," : ",g)
-    print(AnnotTitle)
-    
-    p <- FeaturePlot(object =  subset(Seuratplot, subset = orig.ident == g),
-                     features = i,
-                     cols = c("grey", "blue"), 
-                     reduction = "harmony_umap",
-                     min.cutoff = 'q2',
-                     max.cutoff = 'q98',
-                     pt.size = 0.7,
-                     order = FALSE)+
-      ggtitle(g)
 
-    plot_list[[g]] <- p
+marker_lists <- list(
+  Pre_cDC1 = Pre_cDC1,
+  Early_Immature = Early_Immature,
+  Later_Immature = Later_Immature,
+  Early_Mature = Early_Mature,
+  Late_Mature = Late_Mature
+  )
+
+for (celltype in names(marker_lists)) {
+  for (gene in marker_lists[[celltype]]) {
+    p <- FeaturePlot(
+      object = seu,
+      features = gene,
+      split.by = "orig.ident"
+    ) +
+      ggtitle(paste(celltype, "-", gene))
     
-    }
-  p_combined <- wrap_plots(plot_list, ncol = 4) +
-    plot_annotation(title = paste("Late-mature:", i))
-  
-  print(p_combined)
+    print(p)
+  }
 }
+
 dev.off()
 
 
