200c0758ba1e3e9658e95f7f5ed0f64e6db7cb0b
Samuel-Howest1 <samuel.gundes@student.howest.be>
2026-06-10 16:41:28 +0200

    Update Biomarkers.R

--- Scripts/Integration and Multimodel/Biomarkers.R
+++ Scripts/Integration and Multimodel/Biomarkers.R
@@ -1,8 +1,27 @@
 
+
+
 library("Seurat")
 library("presto") 
 library("openxlsx")
 library("readxl")
+library("dplyr")
+#################################
+# Functions
+colorSomeCells<-function(clusterMatrix, coordsTable, cellsToColor){
+  clusterMatrix$toColor=FALSE
+  clusterMatrix[cellsToColor,which(colnames(clusterMatrix)=="toColor")]<-TRUE
+  
+  p1 <- ggplot()+
+    geom_point(aes_string(x=colnames(coordsTable)[1],y=colnames(coordsTable)[2], colour=clusterMatrix$toColor), data=coordsTable, size=1) +
+    scale_color_manual(values=c('TRUE'="orangered",'FALSE'="lightgray")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5), legend.position = "right", legend.text=element_text(size=12))
+  
+  return(p1)
+}
+##################################
+
 
 
 # Based on the Pre-processing script
@@ -35,3 +54,31 @@ names(markersList)<-paste0("cluster",0:totalNrClusters)
 ### Write to Excel
 
 write.xlsx(markersList,paste0("Harmony_Marker_RNA_Final.xlsx"))
+
+clusterMatrix<-seuratObj@meta.data
+umapTable<-as.data.frame(seuratObj[['umap']]@cell.embeddings, stringsAsFactors = F)
+#####################################""
+# Cluster 33
+umapSlice<-umapTable %>% dplyr::mutate('cell'=rownames(.)) %>% dplyr::filter(., UMAP_1 > 0)
+wantedCells<-intersect(umapSlice$cell, WhichCells(seuratObj, idents = 12))
+colorSomeCells(clusterMatrix, umapTable, wantedCells)
+
+seuratObj<-SetIdent(object = seuratObj, cells = wantedCells, value = 33)
+DimPlot(seuratObj, reduction = "umap", label = T, label.size = 8)
+
+######################################################################################
+# Cluster 34
+umapSlice<-umapTable %>% dplyr::mutate('cell'=rownames(.)) %>% dplyr::filter(., UMAP_1 > 0)
+wantedCells<-intersect(umapSlice$cell, WhichCells(seuratObj, idents = 28))
+colorSomeCells(clusterMatrix, umapTable, wantedCells)
+
+seuratObj<-SetIdent(object = seuratObj, cells = wantedCells, value = 34)
+DimPlot(seuratObj, reduction = "umap", label = T, label.size = 8)
+####################################################################################
+# Cluster 35
+umapSlice<-umapTable %>% dplyr::mutate('cell'=rownames(.)) %>% dplyr::filter(., UMAP_2 > 2)
+wantedCells<-intersect(umapSlice$cell, WhichCells(seuratObj, idents = 28))
+colorSomeCells(clusterMatrix, umapTable, wantedCells)
+
+seuratObj<-SetIdent(object = seuratObj, cells = wantedCells, value = 35)
+DimPlot(seuratObj, reduction = "umap", label = T, label.size = 8)
