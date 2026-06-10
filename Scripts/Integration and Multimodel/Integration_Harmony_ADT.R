719b7fb64b17611256e9b28ff3580ad4ae17f0fa
Samuel-Howest1 <samuel.gundes@student.howest.be>
2026-06-10 12:16:53 +0200

    Update Integration to ADT to fix bias to JVE datasets

--- Scripts/Integration and Multimodel/Integration_Harmony_ADT.R
+++ Scripts/Integration and Multimodel/Integration_Harmony_ADT.R
@@ -17,35 +17,63 @@ library("openxlsx")
 ####################################################################
 # for script
 output <- "/srv/data/local/samuelg/Output/"
-seuratObjT <- readRDS("/srv/data/local/samuelg/Output/Harmomy/Object/SeuratObjT_Harmony_Treat_Exp.rds")
+seuratObjT <- readRDS("/srv/data/local/samuelg/Output/Harmomy/Object/SeuratObjT_Harmomy_Treat_Exp_V2.rds")
 ###################################################################
-####################################################
+# Adding Metadata
+tech_map <- c(
+  "SAM2" = "A",
+  "SAM3" = "A",
+  "SAM05" = "A",
+  "SAM06" = "A",
+  "SAM016" = "A",
+  "VBO004" = "A",
+  "VBO005" = "A",
+  "VBO006" = "A",
+  "VBO007" = "A",
+  "VBO008" = "A",
+  "VBO009" = "A",
+  "VBO010" = "A",
+  "VBO011" = "A",
+  "VBO012" = "A",
+  "JVE008" = "B",
+  "JVE010" = "B"
+)
+seuratObjT$tech <- unname(tech_map[seuratObjT$orig.ident])####################################################
+
+
+
 #Prepare for integration
 #Joining all Layers First together
 seuratObjT <- JoinLayers(seuratObjT)
+
+# Change default assay for following steps
+DefaultAssay(seuratObjT) <- "ADT"
 
 seuratObjT[["ADT"]] <- split(seuratObjT[["ADT"]],f = seuratObjT$treatment)
 
 seuratObjT <- NormalizeData(seuratObjT, normalization.method = "LogNormalize", scale.factor = 1e4)
 seuratObjT <- FindVariableFeatures(seuratObjT, selection.method = 'vst', nfeatures = 2000)
-all.genes <- rownames(seuratObjT)
-seuratObjT <- ScaleData(seuratObjT, features = all.genes)
+all_ADT <- rownames(seuratObjT[["ADT"]])
+seuratObjT <- ScaleData(seuratObjT, features = all_ADT)
 seuratObjT <- RunPCA(seuratObjT, 
-                     features = VariableFeatures(object = seuratObjT, assay = "ADT"),
+                     features = all_ADT,
                      npcs = 40,
                      ndims.print = 1:5, nfeatures.print = 10,
-                     assay = "RNA",
+                     assay = "ADT",
                      reduction.name = "ADT_pca_int",
                      reduction.key = "ADTPC_int_")
+
+Elbow <- ElbowPlot(seuratObjT, reduction = "ADT_pca_int")
+Elbow
 
 ###############################################################################################
 BenchResult <- bench::mark(
   seuratObjT <- RunHarmony(seuratObjT, 
-                           group.by.vars =c("treatment","experiment"), 
+                           group.by.vars =c("treatment","experiment","tech"), 
                            plot_convergence =TRUE, 
                            reduction.use ="ADT_pca_int", 
                            reduction.save = "harmony_ADT",
-                           theta=c(2,2),
+                           theta=c(2,2,5),
                            sigma=0.2,
                            lambda=1,
                            verbose=TRUE),memory = F
@@ -66,7 +94,7 @@ setwd(output)
 
 # Plots pdf
 Plotslist <-c("orig.ident","experiment","treatment","sctype_classification","seurat_clusters","scDblFinder_class")
-pdf("./Harmomy/Results/Graphs_Harmony_Integration_Treat_Exp_ADT.pdf", width = 10,height = 8)
+pdf("./Harmomy/Results/Graphs_Harmony_Integration_Treat_Exp_Tech_ADT.pdf", width = 10,height = 8)
 for (i in Plotslist){
   
   AnnotTitle <- paste0("Plot Harmony integration ADT: ",i)
@@ -80,5 +108,5 @@ for (i in Plotslist){
 }
 dev.off()
 
-saveRDS(seuratObjT,"./Harmomy/Object/SeuratObjT_Harmony_Treat_Exp_ADT.rds")
+saveRDS(seuratObjT,"./Harmomy/Object/SeuratObjT_Harmony_Treat_Exp_Tech_ADT.rds")
 
--- Scripts/Integration and Multimodel/Multi_model.R
+++ Scripts/Integration and Multimodel/Multi_model.R
@@ -2,7 +2,7 @@
 
 library(Seurat)
 
-seuratobjectMulti <- readRDS("./Output/Harmomy/Object/SeuratObjT_Harmony_Treat_Exp_ADT.rds")
+seuratobjectMulti <- readRDS("./Output/Harmomy/Object/SeuratObjT_Harmony_Treat_Exp_Tech_ADT.rds")
 
 seuratobjectMulti <- FindMultiModalNeighbors(
   seuratobjectMulti, reduction.list = list("harmony", "harmony_ADT"), 
@@ -12,4 +12,4 @@ seuratobjectMulti <- FindMultiModalNeighbors(
 seuratobjectMulti <- RunUMAP(seuratobjectMulti, nn.name = "weighted.nn", reduction.name = "wnn.umap", reduction.key = "wnnUMAP_")
 seuratobjectMulti <- FindClusters(seuratobjectMulti, graph.name = "wsnn", algorithm = 3, resolution = 1, verbose = TRUE)
 
-saveRDS(seuratobjectMulti,"./Output/Harmomy/Object/Harmony_Integration_T_E_ADTV2_Final.rds")
+saveRDS(seuratobjectMulti,"./Output/Harmomy/Object/Harmony_Integration_T_E_T_ADTV2_Final.rds")
